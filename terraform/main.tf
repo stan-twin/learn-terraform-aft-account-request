2f8bec090dcca89bc3c8e101a8d1b097c724237f
Stan Glover <stanley.glover@twinhealth.com>
2023-10-04 13:48:23 -0700

    Create sandbox-aft Control Tower account

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,32 @@ module "sandbox" {
 
   account_customizations_name = "sandbox"
 }
+
+
+module "sandbox" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "2test+stanleyglover560@gmail.com"
+    AccountName               = "sandbox-aft1"
+    ManagedOrganizationalUnit = "Learn AFT"
+    SSOUserEmail              = "stanleyglover560@gmail.com"
+    SSOUserFirstName          = "Sandbox"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
